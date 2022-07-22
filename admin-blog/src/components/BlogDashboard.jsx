d87e4d7058b50ba892413aca47542612938466bf
ManfredMT <amtmatian@outlook.com>
2022-07-22 15:52:14 +0800

    堆叠图

--- admin-blog/src/components/BlogDashboard.jsx
+++ admin-blog/src/components/BlogDashboard.jsx
@@ -1,56 +1,46 @@
-import { useSelector, useDispatch } from "react-redux";
-import { useEffect, useRef, useMemo } from "react";
-import { checkJWT } from "../features/auth/authSlice";
-import WordCloud from "./WordCloud";
-import style from "../css/BlogDashboard.module.css";
-import { Link } from "react-router-dom";
-import { Calendar } from "antd";
-import "moment/locale/zh-cn";
+import { Calendar, message as antMessage } from "antd";
 import locale from "antd/es/date-picker/locale/zh_CN";
+import "moment/locale/zh-cn";
+import { useEffect, useMemo, useRef } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { useMediaQuery } from "react-responsive";
+import { Link } from "react-router-dom";
+import style from "../css/BlogDashboard.module.css";
+import { getPosts, reset } from "../features/posts/postSlice";
+import WordCloud from "./WordCloud";
+import * as echarts from 'echarts/core';
+import { LineChart } from 'echarts/charts';
 import {
-  DatePicker,
-  Divider,
-  Empty,
-  Input,
-  message as antMessage,
-  Modal,
-  Pagination,
-  Radio,
-  Select,
-  Spin,
-} from "antd";
-import { deletePost, getPosts, reset } from "../features/posts/postSlice";
+  TitleComponent,
+  TooltipComponent,
+  GridComponent,
+  DatasetComponent,
+  TransformComponent,
+  LegendComponent
+} from 'echarts/components';
+import { LabelLayout, UniversalTransition } from 'echarts/features';
+import { SVGRenderer } from 'echarts/renderers';
 
-import HCenterSpin from "./HCenterSpin";
-import usePrevious from "../hooks/usePrevious";
 import {
-  reset as resetComments,
   getComments,
+  reset as resetComments,
 } from "../features/comments/commentSlice";
+import usePrevious from "../hooks/usePrevious";
+import HCenterSpin from "./HCenterSpin";
 
-// let AllPosts = [];
-// for (let j = 0; j <= 56; j++) {
-//   const fakeTags = [...Array(5).keys()].map(
-//     (i) => `标签tag-${parseInt((j + i + 1) % 12)}`
-//   );
-//   const dateObj = new Date();
-//   dateObj.setDate(dateObj.getDate() - (j + 1));
 
-//   const fakeAuthor = j >= 30 ? "author2" : "admin";
-//   AllPosts.push({
-//     id: `post_${j + 1}`,
-//     title: `JavaScript 的关键功能，比如变量、字符串、数字、数组等(${j + 1})`,
-//     date: dateObj,
-//     tags: fakeTags,
-//     category: `category-${parseInt((j + 1) % 10)}`,
-//     draft: parseInt(j % 10) === 0 ? true : false,
-//     authors: [fakeAuthor],
-//     images: undefined,
-//   });
-// }
-
-// AllPosts = AllPosts.filter((post) => !post.draft);
+echarts.use([
+  TitleComponent,
+  TooltipComponent,
+  GridComponent,
+  DatasetComponent,
+  TransformComponent,
+  LegendComponent,
+  LineChart,
+  LabelLayout,
+  UniversalTransition,
+  SVGRenderer
+])
 
 function getAllTags(posts) {
   let AllTags = [];
@@ -119,7 +109,70 @@ function getDayArchive(posts) {
   return dayArchive;
 }
 
+function getMonthSeries(posts) {
+  const todayDateObject = new Date();
+  const allTagsList = [];
+  const allCategoriesList = [];
+  const dateSeries = [];
+  const postSeries = [];
+  const tagSeries = [];
+  const categorySeries = [];
+  let tagStackNum = 0;
+  let postStackNum = 0;
+  let categoryStackNum = 0;
+  const todayDate = todayDateObject.getDate();
+  
+  
+  //windowEnd.setDate(todayDate-30);
 
+  for(let i=0;i<=30;i+=2) {
+    const windowStart = new Date();
+    const windowEnd = new Date();
+    windowStart.setDate(todayDate-31+i);
+    windowStart.setHours(0,0,0,0);
+    windowEnd.setDate(todayDate-30+i);
+    windowEnd.setHours(0,0,0,0);
+    dateSeries.push(`${windowEnd.getMonth()+1}.${windowEnd.getDate()}`);
+    let windowPosts;
+    if(i===0) {
+      windowPosts = posts.filter((post)=>{
+        const postDateClearTime = post.date.setHours(0,0,0,0);
+        return postDateClearTime<=windowEnd;
+      })
+    }else {
+      windowPosts = posts.filter((post)=>{
+        const postDateClearTime = post.date.setHours(0,0,0,0);
+        return windowStart<=postDateClearTime&&postDateClearTime<=windowEnd;
+      })
+    }
+    for(let j=0;j<windowPosts.length;j++) {
+      const post = windowPosts[j];
+      postStackNum+=1;
+      if(!allCategoriesList.includes(post.category)) {
+        categoryStackNum+=1;
+        allCategoriesList.push(post.category);
+      }
+      for(let k=0;k<post.tags.length;k++) {
+        const tag = post.tags[k];
+        if(!allTagsList.includes(tag)) {
+          tagStackNum+=1;
+          allTagsList.push(tag);
+        }
+      }
+    }
+    postSeries.push(postStackNum);
+    categorySeries.push(categoryStackNum);
+    tagSeries.push(tagStackNum);  
+
+  }
+  return {
+    dateSeries,
+    postSeries,
+    categorySeries,
+    tagSeries,
+  }
+
+}
 
 function BlogDashboard() {
   const isTabletOrMobile = useMediaQuery({ query: "(max-width: 1224px)" });
@@ -240,15 +293,146 @@ function BlogDashboard() {
 
   const preIsSuccess = usePrevious(isSuccess);
   const preIsSucCom = usePrevious(isSuccessCom);
-  console.log("isSuccess: ", isSuccess);
-  console.log("isSuccessCom: ", isSuccessCom);
-  console.log("preIsSuccess: ", preIsSuccess);
-  console.log("preIsSucCom: ", preIsSucCom);
-  console.log("allPosts: ",allPosts);
-  console.log("comments: ",comments)
+  // console.log("isSuccess: ", isSuccess);
+  // console.log("isSuccessCom: ", isSuccessCom);
+  // console.log("preIsSuccess: ", preIsSuccess);
+  // console.log("preIsSucCom: ", preIsSucCom);
+  // console.log("allPosts: ", allPosts);
+  // console.log("comments: ", comments);
 
+  const chartRef = useRef(null);
 
-  return isSuccess&&isSuccessCom ? (
+  const chartsData = useMemo(()=>getMonthSeries(allPosts),[allPosts]);
+  console.log("chartsData",chartsData);
+  
+  useEffect(() => {
+    if (isSuccess && isSuccessCom) {
+       let myChart = echarts.init(chartRef.current);
+      myChart.setOption({
+        color: ['#80FFA5', '#00DDFF', '#37A2FF', '#FF0087', '#FFBF00'],
+        title: {
+          text: '博客动态'
+        },
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'line',
+            label: {
+              backgroundColor: '#6a7985'
+            }
+          }
+        },
+        legend: {
+          right: isTabletOrMobile?10:'10%',
+          data: ['文章数', '分类数', '标签数']
+        },
+        
+        grid: {
+          left: '3%',
+          right: '4%',
+          bottom: '3%',
+          containLabel: true
+        },
+        xAxis: [
+          {
+            type: 'category',
+            boundaryGap: false,
+            data: chartsData.dateSeries
+          }
+        ],
+        yAxis: [
+          {
+            type: 'value'
+          }
+        ],
+        series: [
+          {
+            name: '文章数',
+            type: 'line',
+            stack: 'Total',
+            smooth: true,
+            lineStyle: {
+              width: 0
+            },
+            showSymbol: false,
+            areaStyle: {
+              opacity: 0.8,
+              color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+                {
+                  offset: 0,
+                  color: 'rgb(128, 255, 165)'
+                },
+                {
+                  offset: 1,
+                  color: 'rgb(1, 191, 236)'
+                }
+              ])
+            },
+            emphasis: {
+              focus: 'series'
+            },
+            data: chartsData.postSeries
+          },
+          {
+            name: '分类数',
+            type: 'line',
+            stack: 'Total',
+            smooth: true,
+            lineStyle: {
+              width: 0
+            },
+            showSymbol: false,
+            areaStyle: {
+              opacity: 0.8,
+              color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+                {
+                  offset: 0,
+                  color: 'rgb(0, 221, 255)'
+                },
+                {
+                  offset: 1,
+                  color: 'rgb(77, 119, 255)'
+                }
+              ])
+            },
+            emphasis: {
+              focus: 'series'
+            },
+            data: chartsData.categorySeries
+          },
+          {
+            name: '标签数',
+            type: 'line',
+            stack: 'Total',
+            smooth: true,
+            lineStyle: {
+              width: 0
+            },
+            showSymbol: false,
+            areaStyle: {
+              opacity: 0.8,
+              color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+                {
+                  offset: 0,
+                  color: 'rgb(55, 162, 255)'
+                },
+                {
+                  offset: 1,
+                  color: 'rgb(116, 21, 219)'
+                }
+              ])
+            },
+            emphasis: {
+              focus: 'series'
+            },
+            data: chartsData.tagSeries
+          },
+        ]
+      });
+    }
+  }, [isSuccess, isSuccessCom]);
+
+  return isSuccess && isSuccessCom ? (
     <div className={style["dashboard-body"]}>
       <div className={style["first-row"]}>
         <div className={style["number-card-box"]}>
@@ -273,8 +457,8 @@ function BlogDashboard() {
         <div className={style["word-cloud-box"]}>
           <WordCloud words={allTags} speed={10} />
         </div>
-        <div className={style["archive-card-body"]}>
-          <p className={style["card-title"]}>日历:</p>
+        <div ref={chartRef} id="chart" className={style["archive-card-body"]}>
+          {/* <p className={style["card-title"]}>日历:</p>
           <hr />
           {isTabletOrMobile ? (
             <ul>
@@ -296,7 +480,7 @@ function BlogDashboard() {
               dateCellRender={dateCellRender}
               monthCellRender={monthCellRender}
             />
-          )}
+          )} */}
         </div>
       </div>
 
