1185196366b2a4d6e9ebc068f55c6ad58287ac2a
ManfredMT <amtmatian@outlook.com>
2022-08-18 21:16:26 +0800

    optimize: admin lazy load

--- admin-blog/src/App.js
+++ admin-blog/src/App.js
@@ -2,86 +2,122 @@ import { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import "./App.less";
-import AllCategories from "./components/AllCategories";
-import AllLinks from "./components/AllLinks";
-import AllPosts from "./components/AllPosts.jsx";
-import AllTags from "./components/AllTags";
-import BlogDashboard from "./components/BlogDashboard";
+//import "./App.less";
+//import AllCategories from "./components/AllCategories";
+//import AllLinks from "./components/AllLinks";
+//import AllTags from "./components/AllTags";
+//import BlogDashboard from "./components/BlogDashboard";
 import CategoryNav from "./components/CategoryNav";
-import ChangePassword from "./components/ChangePassword";
+//import ChangePassword from "./components/ChangePassword";
 import CommentNav from "./components/CommentNav";
-import EditLink from "./components/EditLink";
+//import EditLink from "./components/EditLink";
 import LinkNav from "./components/LinkNav.jsx";
 import ManageNav from "./components/ManageNav";
-import NewLink from "./components/NewLink";
-import NewPost from "./components/NewPost.jsx";
-import PostCategory from "./components/PostCategory";
-import PostComments from "./components/PostComments";
+//import NewLink from "./components/NewLink";
+//import NewPost from "./components/NewPost.jsx";
+//import PostCategory from "./components/PostCategory";
+//import PostComments from "./components/PostComments";
 import PostNav from "./components/PostNav";
-import PostTags from "./components/PostTags";
-import RecentComments from "./components/RecentComments";
+//import PostTags from "./components/PostTags";
+//import RecentComments from "./components/RecentComments";
 import RequireAuth from "./components/RequireAuth.jsx";
-import SEOSetting from "./components/SEOSetting";
 import TagNav from "./components/TagNav";
-import BlogSetting from "./components/BlogSetting";
+//import BlogSetting from "./components/BlogSetting";
 import Loading from "./pages/Loading";
-
-const Login = lazy(() => import("./pages/Login"));
+//import AllPosts from "./components/AllPosts";
+import HCenterSpin from "./components/HCenterSpin";
+//import AdminPanel from "./pages/AdminPanel";
+import Login from "./pages/Login";
+//const Login = lazy(() => import("./pages/Login"));
+//const AdminPanel = lazy(() => import("./pages/AdminPanel"));
 const AdminPanel = lazy(() => import("./pages/AdminPanel"));
+const AllPosts = lazy(() => import("./components/AllPosts.jsx"));
+const NewPost = lazy(() => import("./components/NewPost"));
+const AllTags = lazy(()=>import("./components/AllTags"));
+const PostTags = lazy(()=>import("./components/PostTags"));
+const AllCategories = lazy(()=>import("./components/AllCategories"));
+const PostCategory = lazy(()=>import("./components/PostCategory"));
+const AllLinks = lazy(()=>import("./components/AllLinks"));
+const NewLink = lazy(()=>import("./components/NewLink"));
+const EditLink = lazy(()=>import("./components/EditLink"));
+const RecentComments = lazy(()=>import("./components/RecentComments"));
+const PostComments = lazy(()=>import("./components/PostComments"));
+const BlogSetting = lazy(()=>import("./components/BlogSetting"));
+const ChangePassword = lazy(()=>import("./components/ChangePassword"));
+const BlogDashboard = lazy(()=>import("./components/BlogDashboard"));
+
 function App() {
   return (
     <>
       <Router>
-        <Suspense fallback={<Loading />}>
+        <>
           <Routes>
             <Route path="/" element={<Login />}></Route>
             <Route
               path="/manage"
               element={
                 <RequireAuth>
-                  <AdminPanel />
+                  <Suspense fallback={<Loading />}>
+                    <AdminPanel />
+                  </Suspense>
                 </RequireAuth>
               }
             >
               <Route index element={<ManageNav />} />
-              <Route path="dashboard" element={<BlogDashboard />} />
+              <Route path="dashboard" element={<Suspense fallback={<Loading />}><BlogDashboard /></Suspense>} />
+
               <Route path="post" element={<PostNav />}>
-                <Route path="all-posts" element={<AllPosts />} />
-                <Route path="new-post" element={<NewPost />} />
+                <Route
+                  path="all-posts"
+                  element={
+                    <Suspense fallback={<HCenterSpin />}>
+                      <AllPosts />
+                    </Suspense>
+                  }
+                />
+                <Route path="new-post" element={<Suspense fallback={<Loading />}><NewPost /></Suspense>} />
               </Route>
               <Route path="tag" element={<TagNav />}>
-                <Route path="all-tags" element={<AllTags />} />
-                <Route path="post-tags" element={<PostTags />} />
+                <Route path="all-tags" element={<Suspense fallback={<Loading />}><AllTags /></Suspense>} />
+                <Route path="post-tags" element={<Suspense fallback={<Loading />}><PostTags /></Suspense>} />
               </Route>
               <Route path="category" element={<CategoryNav />}>
-                <Route path="all-categories" element={<AllCategories />} />
-                <Route path="post-category" element={<PostCategory />} />
+                <Route path="all-categories" element={<Suspense fallback={<Loading />}><AllCategories /></Suspense>} />
+                <Route path="post-category" element={<Suspense fallback={<Loading />}><PostCategory /></Suspense>} />
               </Route>
               <Route path="link" element={<LinkNav from="/manage/link" />}>
-                <Route path="all-links" element={<AllLinks />} />
-                <Route path="new-link" element={<NewLink />} />
-                <Route path=":linkId" element={<EditLink />} />
+                <Route path="all-links" element={<Suspense fallback={<Loading />}><AllLinks /></Suspense>} />
+                <Route path="new-link" element={<Suspense fallback={<Loading />}><NewLink /></Suspense>} />
+                <Route path=":linkId" element={<Suspense fallback={<Loading />}><EditLink /></Suspense>} />
                 <Route index element={<LinkNav from="/manage/link/" />} />
               </Route>
               <Route path="comment" element={<CommentNav />}>
-                <Route path="recent-comments" element={<RecentComments />} />
-                <Route path="post-comments" element={<PostComments />} />
+                <Route path="recent-comments" element={<Suspense fallback={<Loading />}><RecentComments /></Suspense>} />
+                <Route path="post-comments" element={<Suspense fallback={<Loading />}><PostComments /></Suspense>} />
               </Route>
-              
-              <Route path="setting" element={<BlogSetting />} />
-              <Route path="change-password" element={<ChangePassword />} />
+
+              <Route path="setting" element={<Suspense fallback={<Loading />}><BlogSetting /></Suspense>} />
+              <Route path="change-password" element={<Suspense fallback={<Loading />}><ChangePassword /></Suspense>} />
+
               <Route
                 path="*"
                 element={
                   <main style={{ padding: "2rem" }}>
-                    <p>404页面不存在</p>
+                    <p>页面不存在</p>
                   </main>
                 }
               />
             </Route>
+            <Route
+              path="*"
+              element={
+                <main style={{ padding: "2rem" }}>
+                  <p>页面不存在</p>
+                </main>
+              }
+            />
           </Routes>
-        </Suspense>
+        </>
       </Router>
       <ToastContainer />
     </>
