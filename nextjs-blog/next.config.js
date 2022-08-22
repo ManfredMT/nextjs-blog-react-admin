cb38bc226e8274311cb5c6cde6e33eea038bcdc9
ManfredMT <amtmatian@outlook.com>
2022-08-22 14:54:18 +0800

    update: nextjs config

--- nextjs-blog/next.config.js
+++ nextjs-blog/next.config.js
@@ -1,7 +1,57 @@
 /** @type {import('next').NextConfig} */
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+
 const nextConfig = {
+  i18n: {
+    locales: ["zh"],
+    defaultLocale: "zh",
+  },
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: ['localhost']
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:slug*',
+        destination: `http://localhost:${process.env.PORT}/api/:slug*`
+      },
+    ]
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Referrer-Policy',
+            value: 'strict-origin-when-cross-origin',
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'X-DNS-Prefetch-Control',
+            value: 'on',
+          },
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=31536000; includeSubDomains',
+          },
+          
+        ],
+      },
+    ]
+  },
+  
 }
 
-module.exports = nextConfig
+module.exports = withBundleAnalyzer(nextConfig)
