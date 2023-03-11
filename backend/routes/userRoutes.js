fd6591f2b8e5a672a173352b06f1f78b808f10a8
ManfredMT <amtmatian@outlook.com>
2023-03-11 15:01:12 +0800

    add login limit

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,19 @@
-const express = require('express')
-const router = express.Router()
-const {loginUser,getMe,updatePassword} = require('../controllers/userController')
-const {protect} = require('../middleware/authMiddleware')
+const express = require('express');
+const router = express.Router();
+const {loginUser,getMe,updatePassword} = require('../controllers/userController');
+const {protect} = require('../middleware/authMiddleware');
+const rateLimit = require("express-rate-limit");
+
+const limiterLogin = rateLimit({
+    windowMs: 30 * 60 * 1000, // ms
+    max: 5, // Limit each IP to xx requests per `window`
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  });
 
 //router.post("/", registerUser)
-router.post("/login", loginUser)
-router.get("/me",protect, getMe)
+router.post("/login",limiterLogin, loginUser);
+router.get("/me",protect, getMe);
 router.put("/update-password",protect,updatePassword);
 
 
