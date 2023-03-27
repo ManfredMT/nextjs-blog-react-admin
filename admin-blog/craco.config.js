2bd9d3b4acd7ea1abe28ca6b42d1424677e66369
ManfredMT <amtmatian@outlook.com>
2023-03-27 10:59:03 +0800

    remove smp add br

--- admin-blog/craco.config.js
+++ admin-blog/craco.config.js
@@ -1,15 +1,15 @@
 const CracoLessPlugin = require("craco-less");
 const { whenProd } = require("@craco/craco");
+const zlib = require("zlib");
 // const BundleAnalyzerPlugin =
 //   require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 const CompressionWebpackPlugin = require("compression-webpack-plugin");
 
 // 查看打包时间
-const SpeedMeasurePlugin = require("speed-measure-webpack-plugin");
-const smp = new SpeedMeasurePlugin();
+// const SpeedMeasurePlugin = require("speed-measure-webpack-plugin");
+// const smp = new SpeedMeasurePlugin();
 
 const overrideConfig = {
-
   plugins: [
     {
       plugin: CracoLessPlugin,
@@ -38,26 +38,45 @@ const overrideConfig = {
       ],
     ],
   },
-  webpack: smp.wrap({
-    plugins: whenProd(()=>[
-      //new BundleAnalyzerPlugin(),
-      new CompressionWebpackPlugin({
+  webpack: {
+    plugins: whenProd(
+      () => [
+        //new BundleAnalyzerPlugin(),
+        new CompressionWebpackPlugin({
           filename: "[path][base].gz",
-          algorithm: 'gzip',// 使用gzip压缩
+          algorithm: "gzip", // 使用gzip压缩
           test: new RegExp(
-              '\\.(js|css)$' // 压缩 js 与 css
+            "\\.(js|css|html)$" // 压缩 js 与 css
           ),
-          threshold: 8192,// 资源文件大于8192B时会被压缩
+          threshold: 8192, // 资源文件大于8192B时会被压缩
           minRatio: 0.8, // 最小压缩比达到0.8时才会被压缩
           deleteOriginalAssets: false,
-        })
-    ],[]),
+        }),
+        new CompressionWebpackPlugin({
+          filename: "[path][base].br",
+          algorithm: "brotliCompress", // 使用brotli算法压缩
+          test: new RegExp(
+            "\\.(js|css|html|svg)$" // 压缩 js,html,css,svg
+          ),
+          compressionOptions: {
+            params: {
+              [zlib.constants.BROTLI_PARAM_QUALITY]: 11,
+            },
+          },
+          threshold: 8192,
+          minRatio: 0.8,
+          deleteOriginalAssets: false,
+        }),
+      ],
+      []
+    ),
     configure: whenProd(() => {
       return {
         mode: "production",
+        devtool: "nosources-source-map",
       };
     }, {}),
-  }),
+  },
 };
 
 module.exports = overrideConfig;
